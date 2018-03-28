47e0bc483ba01b4f68c3d454b8e8cec90490669c
Alexander Panayotov <alexander.panayotov@gmail.com>
2018-03-28 14:38:59 +0300

    Add initial version

---
+++ README.md
@@ -0,0 +1,21 @@
+# Infinite Scroll
+
+A vanilla ES2017 module for extracting query arguments from url.
+
+## Install
+
+```
+npm i @three11/extract-query-arg
+```
+
+or
+
+```
+yarn add @three11/extract-query-arg
+```
+
+## License
+
+GNU GENERAL PUBLIC LICENSE
+Version 3, 29 June 2007
+
---
+++ index.js
@@ -0,0 +1,14 @@
+export const extractQueryArg = (queryArg, url = window.location.href) => {
+	queryArg = queryArg.replace(/[\[\]]/g, '\\$&');
+	const matches = new RegExp(`[?&]${queryArg}(=([^&#]*)|&|#|$)`).exec(url);
+
+	if (!matches) {
+		return null;
+	}
+
+	if (!matches[2]) {
+		return '';
+	}
+
+	return decodeURIComponent(matches[2].replace(/\+/g, ' '));
+};
